f4bc41f2be46a326ef1ccac0281dda2ecbc37673
emmanuel-e <68557108+emmanuel-e@users.noreply.github.com>
2020-07-20 21:57:25 +0530

    KMCSS

---
+++ index_files/script_011.js
@@ -0,0 +1,10 @@
+(function(props,context) {
+        Core.utils.renderBootstrap({
+          elId:'bs-9',
+          componentName:'PopupModal',
+          props:props,
+          context:context,
+          contextKey:'context-bs-1',
+          radpack:""
+        });
+      })({"preset":"popup1","id":"713445f2-d4b6-4578-a23a-ea684014cefb","websiteId":"9b839ca8-f41e-4759-8b07-b4c6ce51d3d6","lastPublished":"2020-07-19T09:35:31.627Z","enabled":false,"popupTitle":"Join us for online Worship Service","popupDescription":"{\"blocks\":[{\"key\":\"7skpt\",\"text\":\" 9:30 am | Sunday | 10th May 2020\",\"type\":\"unstyled\",\"depth\":0,\"inlineStyleRanges\":[],\"entityRanges\":[],\"data\":{\"fontScale\":0}}],\"entityMap\":{}}","cta":{"enabled":true,"label":"WATCH ONLINE","pageId":"28d8f729-fa84-40aa-ab32-39c146c4b422","linkId":"de1ddb2c-e8f0-433f-bcf8-45b911aefa25","linkType":"internal"},"widgetId":"713445f2-d4b6-4578-a23a-ea684014cefb","section":"default","category":"neutral","locale":"en-IN","renderMode":"PUBLISH"},{"widgetId":"713445f2-d4b6-4578-a23a-ea684014cefb","widgetType":"POPUP","widgetPreset":"popup1","section":"default","category":"neutral","fontSize":"medium","fontFamily":"alternate","themeOverrides":{}});
